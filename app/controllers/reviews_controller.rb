fe497368a6d63c8a47b7875b9484a11ce15897d5
Gamoundo <akilgg@gmail.com>
2020-09-10 16:23:39 -0400

    one review per game

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,13 +5,23 @@ class ReviewsController < ApplicationController
     end
 
     def create
-        review = Review.create(
+        reviews = Review.select{|review| review.game_id == params[:game_id]}
+        review = reviews.select{|review| review.user_id === params[:user_id]}
+        
+        if review.length > 0
+            render json: {error: "Only one review per game"}
+        else
+            review = Review.create(
             rating: params[:rating], 
             body: params[:body], 
             user_id: params[:user_id], 
             game_id: params[:game_id]
             )
             render json: review
+        end
+        
+        
+        
 
     end
 
